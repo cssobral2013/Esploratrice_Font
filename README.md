e2fd6e3ff46d917ccfcf9a78a506a709511753e9
Cristiano Sobral <cssobral2013@gmail.com>
2025-09-03 14:18:17 -0300

    Update v1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Esploratrice_Font
 Is developed from and inspired by TASA Explorer
+
+Main changes:
+- Based off Version 1 of TASA Explorer
+- Question mark
+- Two additional weights and italics
+- Language support
---
+++ generate-woff-woff2.py
@@ -0,0 +1,254 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+    
+srcPath  = './ttf/Esploratrice-ExtraLight.ttf'
+woffPath = './web/Esploratrice-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Esploratrice-ExtraLightItalic.ttf'
+woffPath = './web/Esploratrice-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+   
+srcPath  = './ttf/Esploratrice-Light.ttf'
+woffPath = './web/Esploratrice-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-LightItalic.ttf'
+woffPath = './web/Esploratrice-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)       
+    
+srcPath  = './ttf/Esploratrice-Regular.ttf'
+woffPath = './web/Esploratrice-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-Italic.ttf'
+woffPath = './web/Esploratrice-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-Medium.ttf'
+woffPath = './web/Esploratrice-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-MediumItalic.ttf'
+woffPath = './web/Esploratrice-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+srcPath  = './ttf/Esploratrice-SemiBold.ttf'
+woffPath = './web/Esploratrice-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-SemiBoldItalic.ttf'
+woffPath = './web/Esploratrice-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath) 
+    
+srcPath  = './ttf/Esploratrice-Bold.ttf'
+woffPath = './web/Esploratrice-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-BoldItalic.ttf'
+woffPath = './web/Esploratrice-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-ExtraBold.ttf'
+woffPath = './web/Esploratrice-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-ExtraBoldItalic.ttf'
+woffPath = './web/Esploratrice-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-Black.ttf'
+woffPath = './web/Esploratrice-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Esploratrice-BlackItalic.ttf'
+woffPath = './web/Esploratrice-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+ 
+srcPath  = './ttf/Esploratrice-ExtraLight.ttf'
+woff2Path = './web/Esploratrice-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Esploratrice-ExtraLightItalic.ttf'
+woff2Path = './web/Esploratrice-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+ 
+srcPath  = './ttf/Esploratrice-Light.ttf'
+woff2Path = './web/Esploratrice-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-LightItalic.ttf'
+woff2Path = './web/Esploratrice-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-Regular.ttf'
+woff2Path = './web/Esploratrice-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-Italic.ttf'
+woff2Path = './web/Esploratrice-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-Medium.ttf'
+woff2Path = './web/Esploratrice-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-MediumItalic.ttf'
+woff2Path = './web/Esploratrice-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+srcPath  = './ttf/Esploratrice-SemiBold.ttf'
+woff2Path = './web/Esploratrice-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-SemiBoldItalic.ttf'
+woff2Path = './web/Esploratrice-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+ 
+srcPath  = './ttf/Esploratrice-Bold.ttf'
+woff2Path = './web/Esploratrice-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-BoldItalic.ttf'
+woff2Path = './web/Esploratrice-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-ExtraBold.ttf'
+woff2Path = './web/Esploratrice-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-ExtraBoldItalic.ttf'
+woff2Path = './web/Esploratrice-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-Black.ttf'
+woff2Path = './web/Esploratrice-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Esploratrice-BlackItalic.ttf'
+woff2Path = './web/Esploratrice-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
